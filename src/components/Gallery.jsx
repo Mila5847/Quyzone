3f691d0363c7bc2e7d36f29a4e54b6e8a8627071
Mila5847 <mila.kehayova@abv.bg>
2025-07-06 11:23:15 +0300

    improved carrousel style

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -2,16 +2,19 @@ import ImageCarousel from './ImageCarousel';
 
 function Gallery() {
   const images = [
+  '/images/sample_image_2.jpg',
   '/images/sample_image_1.jpg',
   '/images/sample_image_2.jpg',
+  '/images/sample_image_1.jpg',
+  '/images/sample_image_2.jpg',
+  '/images/sample_image_1.jpg',
 ];
 
-
   return (
-    <section>
+    <>
       <h2>Gallery</h2>
       <ImageCarousel images={images} />
-    </section>
+    </>
   );
 }
 
--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -3,32 +3,38 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import { Navigation, Pagination, EffectCoverflow } from 'swiper/modules';
 import '../ImageCarousel.scss';
 
 function ImageCarousel({ images }) {
   return (
     <div className="carousel-container">
-      <Swiper
-        modules={[Navigation, Pagination, Autoplay]}
-        navigation
-        pagination={{ clickable: true }}
-        loop={true}
-        autoplay={{ delay: 3000 }}
-        spaceBetween={30}
-        slidesPerView={1}
-        className="styled-swiper"
-      >
-        {images.map((src, idx) => (
-          <SwiperSlide key={idx}>
-            <img
-              src={src}
-              alt={`Slide ${idx + 1}`}
-              className="carousel-image"
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+       <Swiper
+ watchSlidesProgress={true}
+  effect="coverflow"
+  grabCursor={true}
+  centeredSlides={true}
+  slidesPerView="auto"
+  loop={true}
+  navigation
+  initialSlide={0}
+  pagination={{ clickable: true }}
+  coverflowEffect={{
+    rotate: 0,
+    stretch: 0,
+    depth: 150,
+    modifier: 2.5,
+    slideShadows: false,
+  }}
+  modules={[EffectCoverflow, Pagination, Navigation]}
+  className="styled-swiper"
+>
+  {images.map((src, idx) => (
+    <SwiperSlide key={idx} className="carousel-slide">
+      <img src={src} alt={`Slide ${idx + 1}`} className="carousel-image" />
+    </SwiperSlide>
+  ))}
+</Swiper>
     </div>
   );
 }
