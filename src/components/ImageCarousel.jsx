1480b07b2f615bf8cc6f413e8780f04697da9e50
Mila5847 <mila.kehayova@abv.bg>
2025-07-15 23:35:25 +0300

    new idea for gallery view

--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -1,41 +1,55 @@
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
 
-import { Navigation, Pagination, EffectCoverflow } from 'swiper/modules';
-import '../styles/components/_ImageCarousel.scss';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/navigation';
+import 'swiper/css/thumbs';
+
+// import required modules
+import { FreeMode, Navigation, Thumbs } from 'swiper/modules';
 
 function ImageCarousel({ images }) {
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+
   return (
-    <div className="carousel-container">
+    <>
       <Swiper
-       watchSlidesProgress={true}
-       effect="coverflow"
-       grabCursor={true}
-       centeredSlides={true}
-       slidesPerView="auto"
-       loop={true}
-       navigation
-       initialSlide={0}
-       pagination={{ clickable: true }}
-       coverflowEffect={{
-         rotate: 0,
-         stretch: 0,
-         depth: 150,
-         modifier: 2.5,
-         slideShadows: false,
-       }}
-       modules={[EffectCoverflow, Pagination, Navigation]}
-       className="styled-swiper"
-       >
-        {images.map((src, idx) => (
-          <SwiperSlide key={idx} className="carousel-slide">
-            <img src={src} alt={`Slide ${idx + 1}`} className="carousel-image" />
+        style={{
+          '--swiper-navigation-color': '#fff',
+          '--swiper-pagination-color': '#fff',
+        }}
+        spaceBetween={10}
+        navigation={true}
+        thumbs={{ swiper: thumbsSwiper }}
+        modules={[FreeMode, Navigation, Thumbs]}
+        className="mySwiper2"
+      >
+         {images.map((src, idx) => (
+          <SwiperSlide key={idx}>
+            <img src={src} alt={`Slide ${idx + 1}`} />
           </SwiperSlide>
         ))}
       </Swiper>
-    </div>
+      <Swiper
+        onSwiper={setThumbsSwiper}
+        spaceBetween={10}
+        slidesPerView={4}
+        freeMode={true}
+        watchSlidesProgress={true}
+        modules={[FreeMode, Navigation, Thumbs]}
+        className="mySwiper"
+      >
+      
+       {images.map((src, idx) => (
+          <SwiperSlide key={idx}>
+            <img src={src} alt={`Slide ${idx + 1}`} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
   );
 }
 
