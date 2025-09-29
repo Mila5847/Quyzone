f4e1054d3c9aed4437ed62e828b70a348282fae6
Mila5847 <mila.kehayova@abv.bg>
2025-09-29 19:34:54 +0300

    cleanup

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,21 +1,21 @@
-import ImageCarousel from './ImageCarousel';
+// import ImageCarousel from './ImageCarousel';
 
-function Gallery() {
-  const images = [
-    '/images/sample_image_2.jpg',
-    '/images/sample_image_1.jpg',
-    '/images/sample_image_2.jpg',
-    '/images/sample_image_1.jpg',
-    '/images/sample_image_2.jpg',
-    '/images/sample_image_1.jpg',
-  ];
+// function Gallery() {
+//   const images = [
+//     '/images/sample_image_2.jpg',
+//     '/images/sample_image_1.jpg',
+//     '/images/sample_image_2.jpg',
+//     '/images/sample_image_1.jpg',
+//     '/images/sample_image_2.jpg',
+//     '/images/sample_image_1.jpg',
+//   ];
 
-  return (
-    <>
-      <h2>Gallery</h2>
-      <ImageCarousel images={images} />
-    </>
-  );
-}
+//   return (
+//     <>
+//       <h2>Gallery</h2>
+//       <ImageCarousel images={images} />
+//     </>
+//   );
+// }
 
-export default Gallery;
+// export default Gallery;
