25c64408160877545fbc0efffca2c2699d96f7c4
Quy <tranquylisor@yahoo.com>
2025-10-02 03:21:53 -0400

    just some timeline text

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -59,17 +59,14 @@ function TimeLine() {
     >
       <h2> Me Quy and the Mechabust</h2>
       <q>To go fast, run alone. To go far, walk together.</q>
-      <p>
-        The Mechabust project started. It was a downtime for me professionally—a perfect time to
-        reflect on how to change the world. I&apos;d like to call myself an artist: besides my
-        passion for games and animation, I just love to create things. I love mechs, I love 3D
-        printing—so this whole thing was a no-brainer.{' '}
-      </p>
+      <p>I've always had cool japanese giants mech robots at heart.  So when a professional "imposed period of reflection" came about, i grabbed the opportunity with both hands to begin my work on the Mecha Bust project. It was born out of my biggest passions: storytelling, drawing, sculpting, mechs, 3D printing, and design. </p>
+      <p>A lot of factors had to line up to push it past the starting line: i had to have the ressources, free time, mental and physical energy and especially the urgency.  I would never have a chance again to do this: design my very own customizable model kit of the bust of a mech.</p>
+      <p>I wasn't sure how or even what it would look like but I knew I'd figure it out somehow.  I'm still figuring it out right now.</p>
 
       <Timeline position="right" sx={{ pl: 0, ml: 0 }}>
         <TimelineItem>
           <TimelineOppositeContent sx={{ pr: 2 }}>
-            <LeftLabel lines={['2022']} />
+            <LeftLabel lines={['2022 Somewhere']} />
           </TimelineOppositeContent>
 
           <WhiteSeparator />
@@ -77,22 +74,21 @@ function TimeLine() {
           {/* 2022 */}
           <TimelineContent sx={{ py: '0px', px: 2 }}>
             <Typography sx={{ width: { xs: '30rem' } }}>
-              I began designing, testing, and iterating slowly with whatever resources I could get
-              my hands on.
+              🌱Laying a foundation for my model kits of the Mech Bust.  I began designing, testing, and iterating slowly with whatever resources I could get my hands on.
             </Typography>
           </TimelineContent>
         </TimelineItem>
 
         <TimelineItem>
           <TimelineOppositeContent sx={{ pr: 2 }}>
-            <LeftLabel lines={['2022']} />
+            <LeftLabel lines={['Summer 2024']} />
           </TimelineOppositeContent>
 
           <WhiteSeparator />
 
           {/* 2022 */}
           <TimelineContent sx={{ py: '0px', px: 2 }}>
-            <Typography sx={{ width: { xs: '30rem' } }}>Pioneer-1</Typography>
+            <Typography sx={{ width: { xs: '30rem' } }}>🚀Pioneer-1: My first public attempt . I submitted it to an international contest. The concept of the Mech Bust was already alive in my mind and it had a head I could morph into an ice-shipping craft for the contest theme.</Typography>
           </TimelineContent>
         </TimelineItem>
 
@@ -105,7 +101,20 @@ function TimeLine() {
 
           {/* 2022 */}
           <TimelineContent sx={{ py: '0px', px: 2 }}>
-            <Typography sx={{ width: { xs: '30rem' } }}>Pioneer-2</Typography>
+            <Typography sx={{ width: { xs: '30rem' } }}>🔧Didn’t win last year? All the more reason to go at it again. I revisited my original model, but this time I wasn’t starting from scratch. I rebuilt it to be far more modular designed for customization and community remixing.</Typography>
+          </TimelineContent>
+        </TimelineItem>
+
+        <TimelineItem>
+          <TimelineOppositeContent sx={{ pr: 2 }}>
+            <LeftLabel lines={['2022']} />
+          </TimelineOppositeContent>
+
+          <WhiteSeparator />
+
+          {/* 2022 */}
+          <TimelineContent sx={{ py: '0px', px: 2 }}>
+            <Typography sx={{ width: { xs: '30rem' } }}>🎯Time to be set some goals.  Can’t always rely on contests.  I officially decide to launch an MVP and limit it to mech’s head.</Typography>
           </TimelineContent>
         </TimelineItem>
 
@@ -119,12 +128,12 @@ function TimeLine() {
 
           <TimelineContent sx={{ py: '12px', px: 2 }}>
             <Typography variant="h6" sx={{ fontSize: '1.2rem', fontWeight: 'bold', mb: 0.5 }}>
-              Today
+
             </Typography>
             <Typography sx={{ width: { xs: '30rem' } }}>
-              The Mech Head, Pioneer-3 MVP is Live! The base kit is fully designed and ready for you
-              to download. This is our starting point.
+              🔥The Mech Head, Pioneer-3 MVP is Live! The base kit is fully designed and ready for you to download. This is our starting point.  A real standalone product.
             </Typography>
+            <img></img>
           </TimelineContent>
         </TimelineItem>
 
@@ -141,7 +150,7 @@ function TimeLine() {
               Next Step
             </Typography>
             <Typography sx={{ width: { xs: '30rem' } }}>
-              Community building… stay tuned guys!
+              📈Community building.  Lets get the ball rolling … stay tuned guys!
             </Typography>
           </TimelineContent>
         </TimelineItem>
