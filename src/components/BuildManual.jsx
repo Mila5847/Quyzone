49f57d0130334730023844ba0c8e505548ec8997
Quy <tranquylisor@yahoo.com>
2025-10-14 00:13:40 -0400

    mo tweaks

--- src/components/BuildManual.jsx
+++ src/components/BuildManual.jsx
@@ -13,18 +13,18 @@ function BuildManual() {
 
   // 👉 Data for carousel 0
   const carousel0Items = [{ type: 'img', src: '/images/manual/ori_receiver.jpg', alt: 'Slide 1' }];
-  const carousel0Captions = ['Part A-0'];
+  const carousel0Captions = ['Part A-0 (post-curing NOT recommended)'];
 
   // 👉 Data for carousel 1
   const carousel1Items = [
     { type: 'img', src: '/images/manual/pres_coverBot.jpg', alt: 'Slide 1' },
-    { type: 'img', src: '/images/manual/ori_coverBot.jpg', alt: 'Slide 3' },
-    { type: 'video', src: '/videos/manual/anim-coverBotR.mp4', alt: 'Slide 4' },
-    { type: 'video', src: '/videos/manual/anim-coverBotL.mp4', alt: 'Slide 5' },
+    { type: 'img', src: '/images/manual/ori_coverBot.jpg', alt: 'Slide 2' },
+    { type: 'video', src: '/videos/manual/anim-coverBotR.mp4', alt: 'Slide 3' },
+    { type: 'video', src: '/videos/manual/anim-coverBotL.mp4', alt: 'Slide 4' },
   ];
   const carousel1Captions = [
     'Step 2 \u27A4 A-0 + A-1',
-    'Part A-1(R)',
+    'Part A-1(R) (post-curing NOT recommended)',
     'A-1(R) \u27A4 A-0',
     'A-1(L) \u27A4 A-0',
   ];
@@ -46,7 +46,7 @@ function BuildManual() {
   ];
   const carousel2Captions = [
     'Step 3 \u27A4 A-0 + A-3',
-    'Part A-3',
+    'Part A-3 (post-curing NOT recommended)',
     'A-3 \u27A4 A-0',
     'A-3 \u27A4 A-0',
   ];
@@ -60,7 +60,7 @@ function BuildManual() {
       <section>
         <h2>PIONEER.3</h2>
         <h3 className="subassembly-title">
-          SUB ASSEMBLY A{' '}
+          SUB ASSEMBLY A<span className='post-cure-warning'>(post-curing NOT recommended)</span>{' '}
           <img className="overflow-image" src="./images/decorative/assemblyA.png" alt="assemblyA" />
         </h3>
 
